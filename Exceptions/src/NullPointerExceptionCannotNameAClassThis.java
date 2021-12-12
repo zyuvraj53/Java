487f6d8cd2bf90c11271622daf975c68c9cb7236
Yuvraj Singh <zyuvraj53@gmail.com>
2021-12-13 04:14:48 +0530

    .

--- Exceptions/src/NullPointerExceptionCannotNameAClassThis.java
+++ Exceptions/src/NullPointerExceptionCannotNameAClassThis.java
@@ -2,7 +2,7 @@
 public class NullPointerExceptionCannotNameAClassThis {
 	public static void main(String[] args) {
 		String s = null;
-		System.out.println(s); 
+		System.out.println(s);
 		System.out.println(s.length()); 
 	}
 }
